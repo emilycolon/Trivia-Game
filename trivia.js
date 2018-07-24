cbb375dfea23d85fc7146e7c4ae5cba5a9e2f66d
emily colon <emilycolonHIG@gmail.com>
2018-07-24 15:57:00 -0400

    files created, html entered, trivia Q/A entered

---
+++ trivia.js
@@ -0,0 +1,232 @@
+trivia = {
+  supernatural: [
+    {
+      q: 'What girl did Dean go live with when Sam went to hell?',
+      a: 'Lisa',
+      b: 'Cassie',
+      c: 'Ruby',
+      d: 'Bella'
+    },
+    {
+      q: 'What was the first creature you see the Colt kill?',
+      a: 'Vampire',
+      b: 'Werewolf',
+      c: 'Demon',
+      d: 'Ghoul'
+    },
+    {
+      q:
+        'Which supernatural being can be identified easiest through a CCTV camera?',
+      a: 'Shapeshifter',
+      b: 'Vampire',
+      c: 'Ghost',
+      d: 'Wendigo'
+    },
+    {
+      q: 'How many years older than Sam is Dean?',
+      a: 'Four',
+      b: 'Three',
+      c: 'Five',
+      d: 'Two'
+    },
+    {
+      q: 'Who was the demon that killed Mary and Jess?',
+      a: 'Azazel',
+      b: 'Crowley',
+      c: 'Alistair',
+      d: 'Meg'
+    },
+    {
+      q:
+        'The Archangel Gabriel was originally introduced under a different persona, but by what name was the trickster officially known?',
+      a: 'Loki',
+      b: 'Lilith',
+      c: 'Papa Legba',
+      d: 'Coyote'
+    },
+    {
+      q: 'Who brought Castiel back from Purgatory?',
+      a: 'Naomi',
+      b: 'Zachariah',
+      c: 'God',
+      d: 'Michael'
+    },
+    {
+      q: 'In the episode "The Benders", what was killing people?',
+      a: 'Humans',
+      b: 'Vampires',
+      c: 'Rawheads',
+      d: 'Reapers'
+    },
+    {
+      q: 'Who says this in "Yellow Fever"? "I\'ll man the flashlight!"',
+      a: 'Dean',
+      b: 'Sam',
+      c: 'Cas',
+      d: 'Bobby'
+    },
+    {
+      q:
+        'What was the name of Sam\'s girlfriend that died in the "Pilot" episode?',
+      a: 'Jess',
+      b: 'Ruby',
+      c: 'Meg',
+      d: 'Cassie'
+    }
+  ],
+  gameOfThrones: [
+    {
+      q: "What is the name of Jon's direwolf?",
+      a: 'Ghost',
+      b: 'Summer',
+      c: 'Grey Wind',
+      d: 'Lady'
+    },
+    {
+      q:
+        "How many fingertips did Stannis Baratheon chop off of Davos' hand(s)?",
+      a: 'Four',
+      b: 'Three',
+      c: 'Five',
+      d: 'Two'
+    },
+    {
+      q: 'Who is king of Westeros when the the series begins?',
+      a: 'Robert Baratheon',
+      b: 'Eddard Stark',
+      c: 'Tywin Lannister',
+      d: 'Aerys Targaryan'
+    },
+    {
+      q:
+        'What is the name of the continent on which most of the action of "Game of Thrones" takes place?',
+      a: 'Westeros',
+      b: 'Essos',
+      c: 'Iseros',
+      d: 'The Shadow Lands'
+    },
+    {
+      q:
+        'At the end of season 2, Joffrey Baratheon sits on the Iron Throne, but which Baratheon is actually the rightful king?',
+      a: 'Stannis Baratheon',
+      b: 'Renly Baratheon',
+      c: 'Tommen Baratheon',
+      d: 'Myrcella Baratheon'
+    },
+    {
+      q:
+        "Why could Jon leave the Night's Watch, even though his vows were for life?",
+      a: 'He died',
+      b: 'He became King',
+      c: 'He knew nothing',
+      d: 'He wanted to'
+    },
+    {
+      q: 'Who was the Lightning Lord?',
+      a: 'Beric Dondarrion',
+      b: 'Thoros of Myr',
+      c: 'Meryn Trant',
+      d: 'Randyll Tarly'
+    },
+    {
+      q:
+        'In 2011, which actor who plays Tyrion Lannister, won the Golden Globe award for Best Performance by an Actor in a Supporting Role in a Series?',
+      a: 'Peter Dinklage',
+      b: 'Lena Headey',
+      c: 'Nikolaj Coster-Waldau',
+      d: 'Charles Dance'
+    },
+    {
+      q:
+        "Who did Jon execute after his first general meeting as Lord Commander with the men of the Night's Watch?",
+      a: 'Janos Slynt',
+      b: 'Bowen Marsh',
+      c: 'Alliser Thorne',
+      d: 'Samwell Tarly'
+    },
+    {
+      q:
+        'Which of the following types of wine does the wine merchant attempt to poison Daenerys with?',
+      a: 'Arbor Red',
+      b: 'Stormland Red',
+      c: 'Vale White',
+      d: 'Dornish Red'
+    }
+  ],
+  doctorWho: [
+    {
+      q:
+        'When the Doctor meets Amelia Pond, what is it that he eats and actually likes?',
+      a: 'Fish fingers and custard',
+      b: 'An apple',
+      c: 'A banana',
+      d: 'Pie'
+    },
+    {
+      q: 'Which one of the following is given the nickname "Potato Heads"?',
+      a: 'Sontarans',
+      b: 'Cyber men',
+      c: 'Daleks',
+      d: 'Ood'
+    },
+    {
+      q: 'What was Mickey called in the parallel universe?',
+      a: 'Ricky ',
+      b: 'Craig',
+      c: 'Rory',
+      d: 'Adam'
+    },
+    {
+      q:
+        'In "The Christmas Invasion," The Doctor and Rose return to London. Who is the prime minister at the time?',
+      a: 'Harriet Jones ',
+      b: 'Winston Churchill',
+      c: 'Craig Owens',
+      d: 'Donna Noble'
+    },
+    {
+      q: 'Where did The Doctor and Rose first meet Captain Jack?',
+      a: 'World War II',
+      b: 'Gallifrey',
+      c: 'Skaro',
+      d: 'Mars'
+    },
+    {
+      q: 'Who was the 9th Doctor played by?',
+      a: 'Christopher Eccleston ',
+      b: 'David Tennant',
+      c: 'Matt Smith',
+      d: 'Paul McGann'
+    },
+    {
+      q:
+        'Which one of the following villains is also a timelord like the Doctor?',
+      a: 'The Master',
+      b: 'Cyber Men',
+      c: 'Ood',
+      d: 'Abzorbalof'
+    },
+    {
+      q: 'Where is "Doctor Who" mainly filmed?',
+      a: 'Cardiff, Wales',
+      b: 'London, England',
+      c: 'Edinburgh, Scotland',
+      d: 'Cambridge, England'
+    },
+    {
+      q: 'What was the name of "the last human"?',
+      a: 'Cassandra ',
+      b: 'Rose',
+      c: 'Jack',
+      d: 'Sarah'
+    },
+    {
+      q:
+        'Prime Minister of the United Kingdom during the 20th Century. This skilled veteran helped the Doctor defeat the Daleks, and even encountered the Silence in an altered timeline!',
+      a: 'Neville Chamberlain',
+      b: 'Clement Attlee',
+      c: 'Harold Macmillan',
+      d: 'Winston Churchill'
+    }
+  ]
+};
