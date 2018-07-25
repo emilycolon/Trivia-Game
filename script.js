6acd0bdf091c858bb7f7c72d49597f1dff2ef01a
emily colon <emilycolonHIG@gmail.com>
2018-07-25 10:52:14 -0400

    got results to show correctly

--- script.js
+++ script.js
@@ -2,21 +2,6 @@
  * Script for index.html
  */
 
-/*
-
-load page
-
-Show three buttons
-
-Depending on button picked, will grab different sets of questions:
-$('.supernatural') / trivia.supernatural[i].q/a/b/c/d
-$('.game-of-thrones') / trivia.gameOfThrones[i].q/a/b/c/d
-$('.doctor-who') / trivia.doctorWho[i].q/a/b/c/d
-
-
-
-
-*/
 let show;
 let data;
 let question = 0;
@@ -48,8 +33,8 @@ const showChoices = () => {
 const checkUserAnswer = answer => {
   // Pull the correct answer for the question
   let correct = data[question].correctAnswer;
-  // Compare the users answer to the correct answer and push the resulting
-  // info to an array
+  /* Compare the users answer to the correct answer and push the 
+  resulting info to an array */
   if (answer === correct) {
     results.push({
       question: question,
@@ -67,8 +52,47 @@ const checkUserAnswer = answer => {
 };
 
 const showNextQuestion = () => {
-  question++;
-  showQuestion();
+  /* Checks to see what question was displayed 
+  If any remaining questions, will start loop of show question,
+  populate answers, etc again */
+  if (question < 9) {
+    question++;
+    showQuestion();
+  } else {
+    // If no remaining questions, will move on to scoring
+    calculateScore();
+  }
+};
+
+const calculateScore = () => {
+  let numberCorrect = 0;
+  // Loop through the results and tally the number of correct answers
+  for (i = 0; i < results.length; i++) {
+    results[i].correct && numberCorrect++;
+  }
+  showScore(numberCorrect);
+};
+
+const showScore = number => {
+  // Hide trivia div
+  $('.trivia').addClass('hidden');
+  // Show show-score div
+  $('.show-score').removeClass('hidden');
+  // Update span to show number correct
+  $('.number').text(number);
+  // Move on to showResults function
+  showResults();
+};
+const showResults = () => {
+  // Show results div
+  $('.results').removeClass('hidden');
+  // Loop through results and fill in the DOM
+  for (i = 0; i < results.length; i++) {
+    $(`.user-answer${i}`).text(data[i].answers[parseInt(results[i].response)]);
+    $(`.correct-answer${i}`).text(
+      data[i].answers[parseInt(data[i].correctAnswer)]
+    );
+  }
 };
 
 $(document).ready(() => {
@@ -108,7 +132,6 @@ $(document).ready(() => {
     e.preventDefault();
     // Assign selected answer to variable
     let userAnswer = $('input[name=answer-field]:checked').val();
-    console.log(userAnswer);
     checkUserAnswer(userAnswer);
   });
 });
